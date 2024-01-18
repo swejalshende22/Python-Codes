2c997e85ecf2f4ded0d7074c250684c766b9dcb3
swejalshende22 <157021674+swejalshende22@users.noreply.github.com>
2024-01-18 17:46:17 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,6 @@
+Name="swejal"
+Age=20
+Adress="Nagpur"
+Emailid="swejal@gmail.com"
+print("Name=",Name,"Age=",Age,"Adress=",Adress,"Emailid=",Emailid)
+print(F"My{Name=} \n My{Adress=} \n My{Age=} \n My{Emailid=}")      #FANCY STRING
