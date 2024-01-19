32e88ebde7db00c38f442afd6c4dddef54810849
swejalshende22 <157021674+swejalshende22@users.noreply.github.com>
2024-01-19 22:42:58 +0530

    Add files via upload

---
+++ class_time.py
@@ -0,0 +1,12 @@
+Time=input("Enter your Class time:")
+
+if Time=="8am to 11am":
+    print(" your full stack class1")
+elif Time=="11am to 2pm":
+    print("your full sack class2")
+elif Time=="2pm to 3pm":
+    print("your java full stack class")
+elif Time=="4pm and 7pm":
+    print("your Data Analyst class")
+else:
+    print("your class Timing is not match") 
