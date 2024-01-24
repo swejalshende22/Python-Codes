cafefc2ef42bfbb90102974021aaa2a5edfa4b3b
swejalshende22 <157021674+swejalshende22@users.noreply.github.com>
2024-01-24 22:01:15 +0530

    Add files via upload

---
+++ Shedule.py
@@ -0,0 +1,12 @@
+your_shedule=input("whats your shedule timing?")
+
+if your_shedule=="5am to 6am":
+    print("This is your morning walk timing")
+elif your_shedule=="7am to 8am":
+    print("This is your Breakfast timing")
+elif your_shedule=="9am to 10am":
+    print("This is your work timing")
+elif your_shedule=="5pm to 7pm":
+    print("This is your classs timing")
+else:
+    print("your timing is not correct")
