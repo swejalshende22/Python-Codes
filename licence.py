14787768172c999457a6a2e70eb9643db7ec384c
swejalshende22 <157021674+swejalshende22@users.noreply.github.com>
2024-01-24 21:59:17 +0530

    Add files via upload

---
+++ licence.py
@@ -0,0 +1,6 @@
+Age=int(input("Enter your Age:"))
+state=input("Enter your state:")
+if Age>=18 or state=="Maharashtra":
+    print("your Eligible for licence")
+else:
+    print("your not Eligible for licence")
