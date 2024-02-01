e1c7d9a25212414fb61bd31b4d4d7460db350044
swejalshende22 <157021674+swejalshende22@users.noreply.github.com>
2024-02-01 16:32:22 +0530

    Add files via upload

---
+++ Keyword _Argument.py
@@ -0,0 +1,9 @@
+#Keyword Argument
+def Data_key(x,y):
+    print("value of x=",x)
+    print("value of y=",y)
+print("without using keyword")
+Data_key(10,20)
+
+print("with using keyword")
+Data_key(x=500,y=700)
