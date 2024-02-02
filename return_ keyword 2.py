b6fbd3d54e6d396d8803e6973bd4a3b3e5c8fa9f
swejalshende22 <157021674+swejalshende22@users.noreply.github.com>
2024-02-02 20:28:29 +0530

    Add files via upload

---
+++ return_ keyword 2.py
@@ -0,0 +1,16 @@
+#return keyword 2
+
+def square(a):
+    print(a**2)
+print("without return keyword")
+square(4)
+
+
+
+
+
+
+def square(a):
+    return print(a**2)
+print("with return keyword")
+square(4)
